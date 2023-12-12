f055e0fbf1eb375957c99139181450e81c43cdf3
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-12 00:19:04 -0300

    Clase 4 Curso de Instalación de React con Vite y TailwindCSS

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,20 +1,29 @@
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import { Home } from "../Home";
 import { MyAccount } from "../MyAccount";
 import { MyOrder } from "../MyOrder";
 import { MyOrders } from "../MyOrders";
-import { Notfound } from "../NotFound";
 import { SignIn } from "../SignIn";
+import { Notfound } from "../NotFound";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/Sign-in", element: <SignIn /> },
+    { path: "/*", element: <Notfound /> },
+  ]);
+
+  return routes;
+};
 
 function App() {
   return (
-    <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <Notfound />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
