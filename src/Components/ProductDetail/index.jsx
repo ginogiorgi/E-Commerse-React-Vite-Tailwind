d537dbf031635ff5f4a14a42445eeab128301cff
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-14 15:57:44 -0300

    Estilización del Product Detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,11 +1,11 @@
 import { XMarkIcon } from "@heroicons/react/24/solid";
 function ProductDetail() {
   return (
-    <aside className="w-[360px] h-5/6 m-2 top-[68px] flex flex-col  fixed right-0 border border-black rounded-lg bg-white">
+    <aside className="w-[360px] h-5/6 m-2 top-[68px] flex-col fixed right-0 rounded-lg bg-black shadow-[0_0_15px_5px] shadow-myGray/40 border">
       <div className="items-center justify-between flex">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
-          <XMarkIcon className="h-6 w-6 text-black stroke-black" />
+          <XMarkIcon className="h-8 w-8 text-white stroke-white" />
         </div>
       </div>
     </aside>
