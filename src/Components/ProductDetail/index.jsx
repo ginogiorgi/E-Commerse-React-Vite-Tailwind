1d5c8bcd3404b7f1a97a55785a0cf62913e02df9
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-14 21:51:42 -0300

    Estilizado elementos product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -9,30 +9,27 @@ function ProductDetail() {
     <aside
       className={`${
         context.isProductDetailOpen ? "" : "hidden"
-      } w-[360px] h-5/6 m-2 top-[68px] flex-col fixed right-0 rounded-lg bg-black shadow-[0_0_15px_5px] shadow-myGray/40 border`}
+      } w-[360px] h-5/6 m-2 top-[68px] flex-col fixed right-0 rounded-lg bg-black shadow-[0_0_15px_7px] shadow-myGray/40`}
     >
-      <div className="items-center justify-between flex">
-        <h2 className="font-medium text-xl">Detail</h2>
-        <div
-          onClick={() => {
-            context.setIsProductDetailOpen(false);
-          }}
-        >
-          <XMarkIcon className="h-8 w-8 text-white stroke-white drop-shadow-[0px_0px_3px_rgba(255,255,255,1)] hover:text-[#FF0000] hover:stroke-[#FF0000] hover:drop-shadow-[0px_0px_5px_rgba(200,0,0,1)]" />
-        </div>
+      <div
+        onClick={() => {
+          context.setIsProductDetailOpen(false);
+        }}
+      >
+        <XMarkIcon className="h-8 w-8 text-black stroke-black drop-shadow-[0px_0px_3px_rgba(0,0,0,1)] hover:text-[#FF0000] hover:stroke-[#FF0000] hover:drop-shadow-[0px_0px_5px_rgba(200,0,0,1)] absolute z-10 right-0" />
       </div>
-      <figure className="bg-white relative w-full h-3/5">
+      <figure className="bg-white relative w-full h-3/5 rounded-t-lg">
         <img
-          className="w-full h-full object-contain bg-white"
+          className="w-full h-full object-contain bg-white rounded-t-lg"
           src={context.productToShow.image ? context.productToShow.image : ""}
           alt="productImage"
         />
       </figure>
       <p className="flex flex-col p-2">
-        <span className="font-medium text-2xl mb-2">
+        <span className="font-medium text-2xl mb-1">
           ${context.productToShow.price}
         </span>
-        <span className="font-medium text-md">
+        <span className="font-medium text-lg mb-2">
           {context.productToShow.title}
         </span>
         <span className="font-medium text-sm">
