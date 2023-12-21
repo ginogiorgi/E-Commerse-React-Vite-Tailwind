0469cecd41a5e3298b71f69f3ea1906af09c73b6
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-20 23:55:55 -0300

    Protección de rutas

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -5,21 +5,39 @@ import { MyOrders } from "../Pages/MyOrders";
 import { MyAccount } from "../Pages/MyAccount";
 import { SignIn } from "../Pages/SignIn";
 import { Notfound } from "../Pages/NotFound";
+import { useContext } from "react";
+import { ShoppingCartContext } from "../Context";
 
 function AppRoutes() {
+  const context = useContext(ShoppingCartContext);
   return useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/men-clothing", element: <Home /> },
-    { path: "/women-clothing", element: <Home /> },
-    { path: "/electronics", element: <Home /> },
-    { path: "/jewelery", element: <Home /> },
-    { path: "/my-order", element: <MyOrder /> },
-    { path: "/my-orders", element: <MyOrders /> },
-    { path: "/my-orders/last", element: <MyOrder /> },
-    { path: "/my-orders/:id", element: <MyOrder /> },
-    { path: "/sign-in", element: <SignIn /> },
-    { path: "/my-account", element: <MyAccount /> },
-    { path: "/*", element: <Notfound /> },
+    { path: "/", element: context.signOut ? <SignIn /> : <Home /> },
+    { path: "/men-clothing", element: context.signOut ? <SignIn /> : <Home /> },
+    {
+      path: "/women-clothing",
+      element: context.signOut ? <SignIn /> : <Home />,
+    },
+    { path: "/electronics", element: context.signOut ? <SignIn /> : <Home /> },
+    { path: "/jewelery", element: context.signOut ? <SignIn /> : <Home /> },
+    { path: "/my-order", element: context.signOut ? <SignIn /> : <MyOrder /> },
+    {
+      path: "/my-orders",
+      element: context.signOut ? <SignIn /> : <MyOrders />,
+    },
+    {
+      path: "/my-orders/last",
+      element: context.signOut ? <SignIn /> : <MyOrder />,
+    },
+    {
+      path: "/my-orders/:id",
+      element: context.signOut ? <SignIn /> : <MyOrder />,
+    },
+    { path: "/sign-in", element: context.signOut ? <SignIn /> : <SignIn /> },
+    {
+      path: "/my-account",
+      element: context.signOut ? <SignIn /> : <MyAccount />,
+    },
+    { path: "/*", element: context.signOut ? <SignIn /> : <Notfound /> },
   ]);
 }
 
